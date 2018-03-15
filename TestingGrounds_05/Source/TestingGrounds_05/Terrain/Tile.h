f62d1897504db014b5f3f3426b566ba25d7b5e8f
Cucsi <cucsijuan@gmail.com>
2018-03-15 14:25:19 -0300

    Cree un Struct para guardar datos como Location, rotstion y scale (se podia usar un FTransform en ve de hacer esto) Se generan spawn positions random y luego se llama a place actor

--- TestingGrounds_05/Source/TestingGrounds_05/Terrain/Tile.h
+++ TestingGrounds_05/Source/TestingGrounds_05/Terrain/Tile.h
@@ -6,6 +6,15 @@
 #include "GameFramework/Actor.h"
 #include "Tile.generated.h"
 
+USTRUCT()
+struct FSpawnPosition
+{
+	GENERATED_USTRUCT_BODY()
+	FVector SpawnLocation; 
+	float Rotation; 
+	float Scale;
+};
+
 class UActorPool;
 
 UCLASS()
@@ -18,6 +27,7 @@ public:
 	ATile();
 	UFUNCTION(BLueprintcallable, Category = "Weapon")
 	void PlaceActors(TSubclassOf<AActor> ToSpawn, int32 MinSpawn = 1, int32 MaxSpawn = 1, float Radius = 500, float MinScale = 1, float MaxScale = 1);
+	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -39,9 +49,11 @@ public:
 private:
 	void PositionNavMeshBoundsVolume();
 
+	TArray<FSpawnPosition> RandomSpawnPositions(int32 MinSpawn, int32 MaxSpawn, float Radius, float MinScale, float MaxScale);
+
 	bool FindEmptyLocation(FVector & OutLocation,float Radius);
 
-	void PlaceActor(TSubclassOf<AActor>ToSpawn, FVector SpawnPoint, float Rotation,float Scale);
+	void PlaceActor(TSubclassOf<AActor>ToSpawn, const FSpawnPosition& SpawnPosition);
 
 	bool CanSpawnAtLocation(FVector Location, float Radius);
 	
